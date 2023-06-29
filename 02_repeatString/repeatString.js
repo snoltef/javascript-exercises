d850d29bb0b6e62c3a28f122fa040a20ee52b55b
Slothgramer <santiagonolte@hotmail.com>
2023-06-29 12:23:22 -0400

    Solve problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let string = "";
+    for(let i=0; i < times; i++)
+    {string += word};
+    return string;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
